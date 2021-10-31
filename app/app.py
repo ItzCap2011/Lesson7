789c553f37dc94ed0573aa5216dd2093f7a0fe03
ItzCap2011 <91814275+ItzCap2011@users.noreply.github.com>
2021-10-31 10:56:56 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_templates, request
 
 app = Flask(__name__)
 
@@ -6,20 +6,23 @@ app = Flask(__name__)
 def hello_world():
     request_type_str = request.method
     if request_type_str == 'GET':
-        return render_template('index.html', href2='')
+        return render_templates('index.html', href2='')
     else:
-        value_soup = 2
-        value_salad = 3
-        value_coffee = 2
-        value_water = 1
-        num_soup = request.form['soup']
-        num_salad = request.form['salad']
-        num_coffee = request.form['coffee']
-        num_water = request.form['water']
-        total_soup = int(num_soup)*value_soup
-        total_salad = int(num_salad)*value_salad
-        total_coffee = int(num_coffee)*value_coffee
-        total_water = int(num_water)*value_water
+        value_regular = 32
+        value_relaxed = 34
+        value_skinny = 30
+        value_slim = 29
+        value_loose = 35
+        num_regular = request.form['regular']
+        num_relaxed = request.form['relaxed']
+        num_skinny = request.form['skinny']
+        num_slim = request.form['slim']
+        num_loose = request.form['loose']
+        total_regular = int(num_regular)*value_regular
+        total_relaxed = int(num_relaxed)*value_relaxed
+        total_skinny = int(num_skinny)*value_skinny
+        total_slim = int(num_slim)*value_slim
+        total_loose = int(num_loose)*value_loose
         
-        final_result = total_soup + total_salad + total_coffee + total_water 
-        return render_template('index.html', href2='soup='+num_soup+', salad='+num_salad+', coffee='+num_coffee+', water='+num_water+', Total = '+str(final_result))
+        final_result = total_regular + total_relaxed + total_skinny + total_slim + total_loose
+        return render_templates('index.html', href2='regular='+num_regular+', relaxed='+num_relaxed+', skinny='+skinny+', slim='+num_slim+', loose='+num_loose+', Total = '+str(final_result))
